2e888cb45c27f40207db39bb1b92ffdb434fc667
almaz <n58773014@gmail.com>
2023-12-11 12:57:38 +0600

    the simple practice of creating a regular toolbar

---
+++ app/src/main/java/com/example/toolbarpractice/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.toolbarpractice
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.toolbarpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Clic ic toolBar", Toast.LENGTH_LONG).show()
+        }
+        binding.toolBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.camera -> {
+                    Toast.makeText(this, "Clic ic camera", Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                R.id.alarm -> {
+                    Toast.makeText(this, "Clic ic alarm", Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+    }
+}
